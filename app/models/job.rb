63a93d860d3fe0e629073d188baf210268e9b52a
Chuck Smith <chuck@pinchaque.com>
2015-05-14 23:52:36 +0000

    Work in progress - creating job run logic

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,14 @@
 class Job < ActiveRecord::Base
   has_many :job_runs
+
+  public:
+
+  def run(batch_date)
+    jr = JobRun.new
+    jr.jobs_id = this.id
+    jr.job_statuses_id =  JobRunStatus.id_from_label(:new)
+    jr.run_start_time = DateTime.now
+    jr.batch_date = batch_date
+    jr.save
+  end
 end
--- app/models/job_run_status.rb
+++ app/models/job_run_status.rb
@@ -1,2 +1,11 @@
 class JobRunStatus < ActiveRecord::Base
+
+  def id_from_label(label)
+    o = this.find_by(label: label)
+    if o.nil?
+      return nil
+    else
+      return o.id
+    end
+  end
 end
