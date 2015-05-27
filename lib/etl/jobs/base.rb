74461030a9f8aa696a7e999a42ecec3f98124354
Chuck Smith <chuck@pinchaque.com>
2015-05-27 21:29:55 +0000

    Add test of reading runs back out from DB

--- lib/etl/jobs/base.rb
+++ lib/etl/jobs/base.rb
@@ -2,22 +2,18 @@ module ETL::Job
 
   # Base class for all ETL jobs
   class Base
-    @job_model = nil
+    def model()
+      # return the model if we already have it cached in this instance
+      return @model unless @model.nil?
 
-    def job_model()
-      # return the job if we already have it cached in this instance
-      return @job unless @job.nil?
-
-      # get the job out of the DB
-      @job = Job.register(self.class.to_s())
+      # get the model out of the DB
+      @model = Job.register(self.class.to_s())
     end
 
     # Runs the job for the batch_date, keeping the status updated and handling
     # exceptions.
     def run(batch_date)
-      @job_model = job_model()
-
-      jr = @job_model.create_run(batch_date)
+      jr = model().create_run(batch_date)
 
       begin
         jr.running()
--- spec/models/job_run_spec.rb
+++ spec/models/job_run_spec.rb
@@ -28,6 +28,21 @@ RSpec.describe JobRun, :type => :model do
     jr = job.run(batch)
 
     # check this object
+    expect(jr.job_id).to eq(job.model.id)
+    expect(jr.status).to eq(:success)
+    expect(jr.run_start_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
+    expect(jr.run_end_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
+    expect(jr.run_end_time.to_i() - jr.run_start_time.to_i()).to be <= 1
+    expect(jr.batch_date).to eq(batch)
+    expect(jr.num_rows_success).to eq(a)
+    expect(jr.num_rows_error).to eq(b)
+    expect(jr.message).to eq(m)
+
+    # now check what's in the db
+    runs = JobRun.where(job_id: job.model.id)
+    expect(runs.count).to eq(1)
+    jr = runs[0]
+    expect(jr.job_id).to eq(job.model.id)
     expect(jr.status).to eq(:success)
     expect(jr.run_start_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
     expect(jr.run_end_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
@@ -49,6 +64,7 @@ RSpec.describe JobRun, :type => :model do
     jr = job.run(batch)
 
     # check this object
+    expect(jr.job_id).to eq(job.model.id)
     expect(jr.status).to eq(:success)
     expect(jr.run_start_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
     expect(jr.run_end_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
@@ -57,7 +73,21 @@ RSpec.describe JobRun, :type => :model do
     expect(jr.num_rows_success).to eq(a)
     expect(jr.num_rows_error).to eq(b)
     expect(jr.message).to eq(m)
-  end
+    
+    # now check what's in the db
+    runs = JobRun.where(job_id: job.model.id)
+    expect(runs.count).to eq(1)
+    jr = runs[0]
+    expect(jr.job_id).to eq(job.model.id)
+    expect(jr.status).to eq(:success)
+    expect(jr.run_start_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
+    expect(jr.run_end_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
+    expect(jr.run_end_time.to_i() - jr.run_start_time.to_i()).to be > 1
+    expect(jr.batch_date).to eq(batch)
+    expect(jr.num_rows_success).to eq(a)
+    expect(jr.num_rows_error).to eq(b)
+    expect(jr.message).to eq(m)
+   end
 
   it "runs job - error" do
     a = 1
@@ -70,6 +100,7 @@ RSpec.describe JobRun, :type => :model do
     jr = job.run(batch)
 
     # check this object
+    expect(jr.job_id).to eq(job.model.id)
     expect(jr.status).to eq(:error)
     expect(jr.run_end_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
     expect(jr.run_start_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
@@ -77,6 +108,19 @@ RSpec.describe JobRun, :type => :model do
     expect(jr.num_rows_success).to be_nil
     expect(jr.num_rows_error).to be_nil
     expect(jr.message).to eq(job.exception)
-  end
+    
+    # now check what's in the db
+    runs = JobRun.where(job_id: job.model.id)
+    expect(runs.count).to eq(1)
+    jr = runs[0]
+    expect(jr.job_id).to eq(job.model.id)
+    expect(jr.status).to eq(:error)
+    expect(jr.run_end_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
+    expect(jr.run_start_time.strftime('%F')).to eq(DateTime.now.strftime('%F'))
+    expect(jr.batch_date).to eq(batch)
+    expect(jr.num_rows_success).to be_nil
+    expect(jr.num_rows_error).to be_nil
+    expect(jr.message).to eq(job.exception)
+   end
 
 end
