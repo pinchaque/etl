a578b63559d20cf87ebe9a8098e500ad752c4a91
Chuck Smith <chuck@pinchaque.com>
2015-05-27 21:50:14 +0000

    Add logging of ETL job running

--- lib/etl/jobs/base.rb
+++ lib/etl/jobs/base.rb
@@ -10,16 +10,29 @@ module ETL::Job
       @model = Job.register(self.class.to_s())
     end
 
+    def logger
+      Rails.logger
+    end
+
     # Runs the job for the batch_date, keeping the status updated and handling
     # exceptions.
     def run(batch_date)
       jr = model().create_run(batch_date)
 
+      log_prefix = "[Job=#{model().class_name} / Batch=#{batch_date}] "
+
       begin
+        logger.info(log_prefix + "Running...")
         jr.running()
         result = run_internal(batch_date)
+        logger.info(log_prefix + "Success! #{result.num_rows_success} rows; "\
+          + "#{result.num_rows_error} errors; #{result.message}")
         jr.success(result)
       rescue Exception => ex
+        logger.error(log_prefix + "Error: #{ex}")
+        ex.backtrace.each do |x|
+          logger.error(log_prefix + "    #{x}")
+        end
         result = Result.new
         result.message = ex.message
         jr.error(result)
