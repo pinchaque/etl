dacc7cc898796372b958f1afadb8a4b0e647f143
Chris Robinson <chris.robinson@outreach.io>
2017-06-06 23:21:59 -0700

    Update to add fiscal attributes

--- lib/etl/input/date_table.rb
+++ lib/etl/input/date_table.rb
@@ -1,11 +1,49 @@
 module ETL::Input
- 
-  class Day < Base
-    attr_accessor :full_date, :day_of_week_number, :day_of_week_name, :day_of_month, :day_of_year, :weekday_flag, :weekend_flag, :week_number, :month_number, :month_name, :quarter, :quarter_month,:year, :year_month, :year_quarter
 
-    def initialize(d) 
+  class FiscalQuarter < Base
+    attr_accessor :quarter_lookup, :quarter_month_num_lookup
+
+    def initialize(fiscal_start_month)
+      @quarter_lookup = {}
+      @quarter_month_num_lookup = {}
+      curr_month = fiscal_start_month
+      curr_quarter = 1
+      curr_quarter_mon_num = 1
+
+      for i in 1..12
+        @quarter_lookup[curr_month] = curr_quarter
+        @quarter_month_num_lookup[curr_month] = curr_quarter_mon_num
+
+        curr_month +=1
+        if (i % 3) == 0
+          curr_quarter +=1
+          curr_quarter_mon_num = 1
+        else
+          curr_quarter_mon_num += 1
+        end
+
+        if curr_month > 12
+          curr_month = 1
+        end
+      end
+      #puts @quarter_lookup.map{|k,v| "#{k}=#{v}"}.join('&')
+      puts @quarter_month_num_lookup.map{|k,v| "#{k}=#{v}"}.join('&')
+    end
+  end
+
+  class Day < Base
+    attr_accessor :fiscal_start_month, :full_date, :day_of_week_number, :day_of_week_name, :day_of_month, :day_of_year, :weekday_flag, :weekend_flag, :week_number, :month_number, :month_name, :quarter, :quarter_month,:year, :year_month, :year_quarter, :fiscal_year, :fiscal_quarter, :fiscal_quarter_month
+
+    def initialize(fiscal_start_month, d, fiscal_map)
+      if fiscal_start_month < 1 || fiscal_start_month > 12
+        raise ArgumentError "Argument is not a valid month between 1 to 12"
+      end
+
       quarter_num = ((d.mon) / 3.0 ).ceil
       quarter_month = (d.mon + 2) % 3  + 1
+      fiscal_quarter_num = fiscal_map.quarter_lookup.fetch(d.mon)
+      fiscal_quarter_mon_num = fiscal_map.quarter_month_num_lookup.fetch(d.mon)
+
       @full_date = d.strftime('%Y/%m/%d')
       @day_of_week_number = d.wday
       @day_of_week_name = d.strftime('%A')
@@ -21,28 +59,41 @@ module ETL::Input
       @year = d.year
       @year_month = d.strftime('%Y/%m')
       @year_quarter = d.strftime("%Y/Q#{quarter_num}")
+      @fiscal_start_month = fiscal_start_month
+      @fiscal_year = self.calculate_fiscal_year(fiscal_start_month, d)
+      @fiscal_quarter = "#{@fiscal_year}/Q#{fiscal_quarter_num}"
+      @fiscal_quarter_month = fiscal_quarter_mon_num
+    end
+
+    def calculate_fiscal_year(fiscal_start_month, d)
+      calc_year = d.year
+      if d.mon < fiscal_start_month
+          calc_year = d.year-1
+      end
+      calc_year
     end
   end
 
   class DateTable < Base
-    attr_accessor :start_date, :end_date
+    attr_accessor :fiscal_start_month, :start_date, :end_date
 
-    def initialize(start_date, end_date)
+    def initialize(fiscal_start_month, start_date, end_date)
+      @fiscal_start_month = fiscal_start_month
       @start_date = start_date
       @end_date = end_date
     end
 
     # Builds the date rows based on the start and end date provided.
     def each_row(batch = ETL::Batch.new)
+      fiscal_map = FiscalQuarter.new(@fiscal_start_month)
       log.debug("Building date table starting from date #{start_date} to #{end_date}\n")
-      current_date = start_date
-      # We are expecting a result like:
+      current_date = @start_date
       days = []
-      while current_date <= end_date
-        days << Day.new(current_date)
+      while current_date <= @end_date
+        days << Day.new(fiscal_start_month, current_date, fiscal_map)
         current_date = current_date.next_day(1)
       end
-      return days
+      days
     end
 
     # Builds the date rows based on the start and end date provided.
--- spec/input/date_table_spec.rb
+++ spec/input/date_table_spec.rb
@@ -1,20 +1,50 @@
+RSpec.describe "FiscalQuarter" do
+  describe ".quarter_lookup" do
+    context "given start month being July verify quarters and month nums are correct" do
+      it "Ensure fiscal quarter is correct start month > 1" do
+        fq = ETL::Input::FiscalQuarter.new(6)
+        expected = {6 => 1, 7 => 1, 8 => 1, 9=> 2, 10=> 2, 11=>2, 12=>3, 1=>3, 2=>3, 3=> 4, 4=> 4, 5=> 4}
+        expected_mon_num = {6 => 1, 7 => 2, 8 => 3, 9=> 1, 10=> 2, 11=>3, 12=>1, 1=>2, 2=>3, 3=> 1, 4=> 2, 5=> 3}
+        for i in 1..12
+          expect(fq.quarter_lookup[i]).to eq expected[i]
+          expect(fq.quarter_month_num_lookup[i]).to eq expected_mon_num[i]
+        end
+      end
+      it "Ensure fiscal quarter is correct start month == 1" do
+        fq = ETL::Input::FiscalQuarter.new(1)
+        expected = {1 => 1, 2 => 1, 3 => 1, 4=> 2, 5=> 2, 6=>2, 7=>3, 8=>3, 9=>3, 10=> 4, 11=> 4, 12=> 4}
+        expected_mon_num = {1 => 1, 2 => 2, 3 => 3, 4=> 1, 5=> 2, 6=>3, 7=>1, 8=>2, 9=>3, 10=> 1, 11=> 2, 12=> 3}
+        for i in 1..12
+          expect(fq.quarter_lookup[i]).to eq expected[i]
+          expect(fq.quarter_month_num_lookup[i]).to eq expected_mon_num[i]
+        end
+      end
+    end
+  end
+end
+
 RSpec.describe "DateTable" do
   describe ".each_row" do
     context "given start date and end date build consecutive rows" do
-      it "Build two consecutive rows from 11/22/2017 to 11/24/2017" do
-        start_date = Date.new(2017, 11, 22)
-        end_date = Date.new(2017, 11, 24)
-        dt = ETL::Input::DateTable.new(start_date, end_date)
+      it "Build two consecutive rows, ensure fiscal year correct" do
+        start_date = Date.new(2017, 10, 01)
+        end_date = Date.new(2018, 12, 01)
+        dt = ETL::Input::DateTable.new(11, start_date, end_date)
         rows = dt.each_row().collect{|x| x}
-        rows.each {|x| puts x}
-        expect(rows.length).to eq 3
+        expect(rows.length).to eq 427
+        row = rows[0]
+        expect(row.fiscal_year).to eq 2016
+        row = rows[40]
+        expect(row.fiscal_year).to eq 2017
+        row = rows[396]
+        expect(row.full_date).to eq "2018/11/01"
+        expect(row.fiscal_year).to eq 2018
       end
       it "Build rows with end_date before start_date" do
         start_date = Date.new(2017, 11, 25)
         end_date = Date.new(2017, 11, 24)
-        dt = ETL::Input::DateTable.new(start_date, end_date)
+        dt = ETL::Input::DateTable.new(1, start_date, end_date)
         rows = dt.each_row().collect{|x| x}
-        rows.each {|x| puts x}
         expect(rows.length).to eq 0
       end
     end
@@ -24,9 +54,10 @@ end
 RSpec.describe "Day" do
   describe ".build_day" do
     context "given date build date row with info" do
-      it "weekday date row should map info from 11/22/2017 to day" do
+      it "Weekday with Quarter starting in Jan" do
         d = Date.new(2017, 11, 22)
-        v = ETL::Input::Day.new(d)
+        fq = ETL::Input::FiscalQuarter.new(1)
+        v = ETL::Input::Day.new(1, d, fq)
         expect(v.full_date).to eq '2017/11/22'
         expect(v.day_of_week_number).to eq 3
         expect(v.day_of_week_name).to eq "Wednesday"
@@ -41,11 +72,15 @@ RSpec.describe "Day" do
         expect(v.year).to eq 2017
         expect(v.year_month).to eq "2017/11"
         expect(v.year_quarter).to eq "2017/Q4"
+        expect(v.fiscal_year).to eq 2017
+        expect(v.fiscal_quarter).to eq "2017/Q4"
+        expect(v.fiscal_quarter_month).to eq 2
       end
 
-      it "weekend date row should map info from 11/26/2017 to day" do
+      it "Weekend with Quarter starting in Dec" do
         d = Date.new(2017, 11, 26)
-        v = ETL::Input::Day.new(d)
+        fq = ETL::Input::FiscalQuarter.new(12)
+        v = ETL::Input::Day.new(12, d, fq)
         expect(v.full_date).to eq '2017/11/26'
         expect(v.day_of_week_number).to eq 0
         expect(v.day_of_week_name).to eq "Sunday"
@@ -60,6 +95,9 @@ RSpec.describe "Day" do
         expect(v.year).to eq 2017
         expect(v.year_month).to eq "2017/11"
         expect(v.year_quarter).to eq "2017/Q4"
+        expect(v.fiscal_year).to eq 2016
+        expect(v.fiscal_quarter).to eq "2016/Q4"
+        expect(v.fiscal_quarter_month).to eq 3
       end
     end
   end
