e3d68b9f9242e684ff29e735638a0a164f3d1b33
Chris Robinson <chris.robinson@outreach.io>
2017-07-24 09:26:39 -0700

    Add fix to allow names to be symbols

--- lib/etl/job/manager.rb
+++ lib/etl/job/manager.rb
@@ -16,30 +16,31 @@ module ETL::Job
     # subclasses to register themselves with a convenient id to represent
     # that subclass. Only registered jobs can be executed.
     def register(id, klass, klass_factory=nil)
-      ETL.logger.debug("Registering job class with manager: #{id} => #{klass}")
+      id_str = id.to_s
+      ETL.logger.debug("Registering job class with manager: #{id_str} => #{klass}")
       if @job_classes.has_key?(id)
-        ETL.logger.warn("Overwriting previous registration of: #{id} => #{@job_classes[id]}")
+        ETL.logger.warn("Overwriting previous registration of: #{id_str} => #{@job_classes[id_str]}")
       end
 
       if !klass_factory.nil?
-        ETL.logger.debug("Registering job class factory with manager: #{id}")
-        if @job_classes.has_key?(id)
-          ETL.logger.warn("Overwriting previous registration of job factory: #{id}")
+        ETL.logger.debug("Registering job class factory with manager: #{id_str}")
+        if @job_classes.has_key?(id_str)
+          ETL.logger.warn("Overwriting previous registration of job factory: #{id_str}")
         end
-        @job_class_factories[id] = klass_factory
+        @job_class_factories[id_str] = klass_factory
       end
 
-      @job_classes[id] = klass
+      @job_classes[id_str] = klass
     end
 
     # Returns the job class registered for the specified ID, or nil if none
     def get_class(id)
-      @job_classes[id]
+      @job_classes[id.to_s]
     end
 
     # Returns the job class factory registered for the specified ID, or nil if none
     def get_class_factory(job_id)
-      @job_class_factories[job_id]
+      @job_class_factories[job_id.to_s]
     end
 
     # Iterates through the loaded classes specified during class initialization.
